fed384d6eb77b622864f59f389b53d1ac8968a23
jinho <jinhoparkseoul@gmail.com>
2022-12-07 01:23:22 +0900

    add data_loader for cifar10 & cifar10_base_model & cifar10.json config

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -14,3 +14,18 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class CIFAR10DataLoader(BaseDataLoader):
+    """
+    CIFAR10 data loader using BaseDataLoader
+    """
+
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.1, num_workers=1, training=True):
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+        self.data_dir = data_dir
+        self.dataset = datasets.CIFAR10(self.data_dir, train=training, download=False, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
